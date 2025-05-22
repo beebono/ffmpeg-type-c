4fbb66379842048c675af931187776e8ec4b5337
Noxwell <78569681+beebono@users.noreply.github.com>
2025-05-21 20:26:47 -0500

    Update version_major.h for Steam Link compatiblity

--- libavcodec/version_major.h
+++ libavcodec/version_major.h
@@ -25,7 +25,7 @@
  * Libavcodec version macros.
  */
 
-#define LIBAVCODEC_VERSION_MAJOR  61
+#define LIBAVCODEC_VERSION_MAJOR  59
 
 /**
  * FF_API_* defines may be placed below to indicate public API that will be
